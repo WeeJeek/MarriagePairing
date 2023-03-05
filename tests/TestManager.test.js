462ab7b0c45fdd35f3d70ef42a05ee79deb95d8a
WeeJeek <wjk960208@gmail.com>
2023-03-05 23:24:00 +0100

    unit test just st up and can compile

---
+++ tests/TestManager.test.js
@@ -0,0 +1,12 @@
+import TestManager from "../utils/TestManager.js"
+
+describe("unit test testing for Test Manager class", () =>{
+  beforeEach(() => {
+    console.log("before test start")
+    var sut = new TestManager()
+  })
+  
+  it("dasf", ()=>{
+    
+  })
+})
--- utils/TestsCollectionManager.js
+++
@@ -1,5 +0,0 @@
-export class TestCollectionManager{
-  constructor(){
-
-  }
-}
