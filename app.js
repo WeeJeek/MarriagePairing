dce483eb91576df1dad61c61942a1cfaca0519d0
WeeJeek <wjk960208@gmail.com>
2023-03-23 17:50:33 +0100

    wrong dependency on TestProgressManager removed

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-import { TestProgressManager } from "./utils/TestProgressManager";
-
 App({
   global_data: {
     user_info: ""
-  },
-  test_progress_manager: new TestProgressManager()
-  
+  }  
 });
