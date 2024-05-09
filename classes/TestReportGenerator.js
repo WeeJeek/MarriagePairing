25713cf9a6ba7f8935c1dbae58a1aa5605a0bb87
WeeJeek <wjk960208@gmail.com>
2024-05-09 23:42:22 +0200

    adapted the structure of calcualtion strategy setup

--- classes/TestReportGenerator.js
+++ classes/TestReportGenerator.js
@@ -6,11 +6,15 @@ import TestCategories  from "../enums/TestCategories"
 
 export default class TestReportCalculator{
   #calculation_strategy = null;
+  #male_result = null;
+  #female_result = null;
   #test_report = {};
   #is_report_successfully_generated = false;
 
   generate_test_report(test_record){
-    this.#store_user_info_to_report();
+    //this.#store_user_info_to_report();
+    this.#male_result = this.#collect_user_result();//need to adjust signature
+    this.#female_result = this.#collect_user_result();
     this.#calculate_test_result(test_record);
 
     return this.#test_report;
@@ -24,32 +28,43 @@ export default class TestReportCalculator{
 
   }
 
+  #merge_test_result_from_2_users(result_male, result_female){
+
+  }
+
+  #collect_user_result(){
+
+  }
+
   #calculate_test_result(test_record){
     if(!test_record){
       return;
     }
 
+    let answers;
+
     Object.keys(test_record).forEach(
       function(test_name){
         if(test_name == TestCategories.MBTI){
           this.#set_strategy(new MBTIResultCalculationStrategy());
-          this.#is_report_successfully_generated = true;
         }
         else if(test_name == TestCategories.FAMILY_ADAPTABILITY_TEST){
           this.#set_strategy(new FamilyAdaptabilityResultCalculationStrategy());
-          this.#is_report_successfully_generated = true;
         }
         else if(test_name == TestCategories.LIFE_PRESSURE_ANALYSIS){
           this.#set_strategy(new LifePressureAnalysisResultCalculationStrategy());
-          this.#is_report_successfully_generated = true;
         }
         else if(test_name == TestCategories.HAPPY_MARRIAGE_ASSESSMENT){
           this.#set_strategy(new HappyMarriageAssessmentResultCalculationStrategy());
-          this.#is_report_successfully_generated = true;
+          answers = this.#merge_test_result_from_2_users(this.#male_result, this.#female_result);
         }
-        
-        const result_of_category = this.#calculation_strategy.calculate_test_result();
-        this.#collect_test_report(result_of_category);
+//现在的问题是，HappyMarriage需要两个结果，怎么样统一格式？
+        if(this.#calculation_strategy != null){
+            const result_of_category = this.#calculation_strategy.calculate_test_result(answers);
+            this.#collect_test_report(result_of_category);
+            this.#is_report_successfully_generated = true;
+            this.#calculation_strategy = null;
+        }
       }
     );
   }
